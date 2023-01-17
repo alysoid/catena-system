870fe5b155871c1753ffbae23376bdae3561113b
Andrea Brandi <me@andreabrandi.com>
2023-01-17 23:17:13 +0100

    build(git): commitlint forces error when no leading blank lines

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,11 @@
-module.exports = { extends: ['@commitlint/config-conventional'] }
+const Configuration = {
+  // Resolve and load @commitlint/config-conventional
+  extends: ['@commitlint/config-conventional'],
+  // Override rules from @commitlint/config-conventional
+  rules: {
+    'body-leading-blank': [2, 'always'],
+    'footer-leading-blank': [2, 'always'],
+  },
+}
+
+module.exports = Configuration
